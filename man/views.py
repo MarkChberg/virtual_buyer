a4a1ec3e983b5e493f27e61337b9a707a2c962bb
wangjiatao <wangjiatao@jd.com>
2018-01-04 20:32:48 +0800

    增加首页映射函数

--- man/views.py
+++ man/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render,HttpResponse,redirect
 
 def man_index(request):
     return render(request, "man/man_index.html", {'content':'欢迎来到管理页面'})
+
+
+def index(request):
+    return render(request, 'index.html')
